a8e383fd81d713897cdbe73634467cbc2d585764
Anna <01153099@pw.edu.pl>
2019-11-14 23:27:31 +0100

    a code to be debugged, but is at least some base

--- seek.c
+++ seek.c
@@ -11,7 +11,6 @@ int main(int argc, char**argv){
 		return EXIT_FAILURE;
 	}
 
-
 	//otwieranie pliku
 	FILE *in = argc > 1 ? fopen( argv[1], "r") : stdin;
 	if( in == NULL){
@@ -19,14 +18,45 @@ int main(int argc, char**argv){
 		return EXIT_FAILURE;
 	}
 
+	//wpisanie pliku do tablicy kopia
+	char *kopia[MAXLINES];
+  	int ile_linii;
+	char buf[BUFSIZE];
+
+	int ile_inii= 0;
+	while( fgets( buf, BUFSIZE, in ) != NULL ) {
+		if( ile_linii < MAXLINES ) {
+			if( (kopia[ile_linii]= malloc( (strlen(buf)+1) * sizeof( char ) )) == NULL ) {
+				fprintf( stderr, "%s: błąd: zbyt wiele danych (brak pamięci)\n", argv[0] );
+				return EXIT_FAILURE;
+			}
+			strcpy( kopia[ile_linii], buf );
+			ile_linii++;
+		} else {
+			fprintf( stderr, "%s: błąd: zbyt wiele linii wejścia\n", argv[0] );
+			return EXIT_FAILURE;
+		}
+	}
+
 	/*zasadnicza czesc do przerobki
 	*zapisac argumenty do tablic(tablicy tablic?)
 	*kolejno wywolywac je do porownania z lines
-	*jesli pattern wystapi, wpisac linie do dst)*/
+	*jesli pattern wystapi, wpisac linie do dst
+	nested loop will do!*/
+
+	char *ptr;
+	char *pattern = argv[2];
+	int i;
+	char *ptr_tab[ile_linii];
+	for(i = 0; i < ile_linii; i++){
+		ptr = strstr(kopia[i] ,pattern);
+		ptr_tab[i] = ptr;
+	}
 
 	int j; 
-	for( j=0; j < ; j++)
-		printf("%s", dst[j]);
-
+	for( j=0; j < ile_linii; j++){
+		if(ptr != NULL)
+			printf("%s", kopia[(int)ptr]);
+        }
 	return EXIT_SUCCESS;
  }
